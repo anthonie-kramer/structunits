46fc5ab54e2bbf183682a91c3df821bdb2935709
Anthonie Kramer <anthoniekramer@gmail.com>
2025-08-12 22:06:33 -0400

    update to lots of stuff

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="structunits",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     description="A Python unit conversion framework with operator overloading",
-    author="Your Name",
-    author_email="your.email@example.com",
-    python_requires=">=3.6",
+    author="Anthonie Kramer",
+    author_email="anthoniekramer@example.com",
+    python_requires=">=3.11",
 )
